1429e792a2cf748a970c6a5ba891ce0972ff424e
tracycrossrvt-gif <tracycrossrvt@gmail.com>
2026-08-01 14:10:23 -0700

    refactor: extract landing page into reusable components

---
+++ components/landing/LandingNavbar.tsx
@@ -0,0 +1,22 @@
+export default function LandingNavbar() {
+  return (
+    <nav className="nav">
+      <div className="nav__wrapper">
+        <figure className="nav__img--mask">
+          <img
+            className="nav__img"
+            src="/assets/logo.png"
+            alt="Summarist logo"
+          />
+        </figure>
+
+        <ul className="nav__list--wrapper">
+          <li className="nav__list nav__list--login">Login</li>
+          <li className="nav__list nav__list--mobile">About</li>
+          <li className="nav__list nav__list--mobile">Contact</li>
+          <li className="nav__list nav__list--mobile">Help</li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
